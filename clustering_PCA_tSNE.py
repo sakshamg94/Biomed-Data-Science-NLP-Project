f3b4b1c3176185f2bf812a5f22ec06e64d1ab060
Saksham Gakhar <sakshamg@stanford.edu>
2021-01-31 15:23:29 -0800

    Create clustering_PCA_tSNE.py

---
+++ clustering_PCA_tSNE.py
@@ -0,0 +1,71 @@
+'''https://towardsdatascience.com/visualising-high-dimensional-datasets-using-pca-and-t-sne-in-python-8ef87e7915b'''
+N = 10000
+
+# ----------------------- PCA
+df_subset = df.loc[rndperm[:N],:].copy()
+data_subset = df_subset[feat_cols].values
+
+pca = PCA(n_components=3)
+pca_result = pca.fit_transform(data_subset)
+
+df_subset['pca-one'] = pca_result[:,0]
+df_subset['pca-two'] = pca_result[:,1] 
+df_subset['pca-three'] = pca_result[:,2]
+
+print('Explained variation per principal component: {}'.format(pca.explained_variance_ratio_))
+
+plt.figure(figsize=(16,10))
+sns.scatterplot(
+    x="pca-one", y="pca-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df.loc[rndperm,:],
+    legend="full",
+    alpha=0.3
+)
+plt.savefig('PCA.png')
+
+# --------------------- tSNE
+time_start = time.time()
+tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+tsne_results = tsne.fit_transform(data_subset)
+print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+
+df_subset['tsne-2d-one'] = tsne_results[:,0]
+df_subset['tsne-2d-two'] = tsne_results[:,1]
+plt.figure(figsize=(16,10))
+sns.scatterplot(
+    x="tsne-2d-one", y="tsne-2d-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df_subset,
+    legend="full",
+    alpha=0.3
+)
+plt.savefig('tSNE.png')
+
+# --------------------- tSNE on PCA redudced data
+pca_50 = PCA(n_components=50)
+pca_result_50 = pca_50.fit_transform(data_subset)
+print('Cumulative explained variation for 50 principal components: {}'\
+	.format(np.sum(pca_50.explained_variance_ratio_)))
+
+time_start = time.time()tsne = TSNE(n_components=2, verbose=0, perplexity=40, n_iter=300)
+tsne_pca_results = tsne.fit_transform(pca_result_50)
+print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
+
+df_subset['tsne-pca50-one'] = tsne_pca_results[:,0]
+df_subset['tsne-pca50-two'] = tsne_pca_results[:,1]
+
+plt.figure(figsize=(16,10))
+ax3 = plt.subplot(1, 3, 3)
+sns.scatterplot(
+    x="tsne-pca50-one", y="tsne-pca50-two",
+    hue="y",
+    palette=sns.color_palette("hls", 10),
+    data=df_subset,
+    legend="full",
+    alpha=0.3,
+    ax=ax3
+)
+plt.savefig('tSNE-pca50.png')
