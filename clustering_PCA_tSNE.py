7511347727f9e97fbb7e231599d4b9d0510af225
Saksham Gakhar <sakshamg@stanford.edu>
2021-03-10 16:16:57 -0800

    PCA+tSNE analysis for 7 classes

--- clustering_PCA_tSNE.py
+++ clustering_PCA_tSNE.py
@@ -1,4 +1,13 @@
 '''https://towardsdatascience.com/visualising-high-dimensional-datasets-using-pca-and-t-sne-in-python-8ef87e7915b'''
+import timeimport numpy as np
+import pandas as pdfrom sklearn.datasets import fetch_mldata
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+%matplotlib inline
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3Dimport seaborn as sns
+
+
 N = 10000
 
 # ----------------------- PCA
